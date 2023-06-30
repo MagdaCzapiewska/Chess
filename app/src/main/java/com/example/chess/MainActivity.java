9c2c41b39d344f4395202ce3325cc89dc0d04409
Magda Czapiewska <magdalena.czapiewska001@wp.pl>
2023-07-01 01:01:58 +0200

    Simple layout

--- app/src/main/java/com/example/chess/MainActivity.java
+++ app/src/main/java/com/example/chess/MainActivity.java
@@ -2,13 +2,62 @@ package com.example.chess;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    DisplayMetrics displayMetrics;
+    int numberOfColumns = 8;
+    int numberOfRows = 8;
+
+
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+        int screenWidth = displayMetrics.widthPixels;
+        int paddingDp = 5;
+
+        LinearLayout boardView = findViewById(R.id.boardView);
+
+        float scale = getResources().getDisplayMetrics().density;
+        int paddingPixel = (int) (paddingDp * scale + 0.5f);
+        int btnWeight  = (int) ((screenWidth - 2 * paddingPixel) / numberOfColumns);
+        int btnHeight = (int) ((screenWidth - 2 * paddingPixel) / numberOfColumns);
+
+        for (int row = 0; row < numberOfRows; row++) {
+            LinearLayout rowLayout = new LinearLayout(this);
+            rowLayout.setOrientation(LinearLayout.HORIZONTAL);
+            rowLayout.setLayoutParams(new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            ));
+            for (int column = 0; column < numberOfColumns; column++) {
+                ImageButton button = new ImageButton(this, null, 0, R.style.SquareStyle);
+                //button.setId(row * numberOfRows + column);
+                button.setId(row * numberOfColumns + column);
+                button.setLayoutParams(new LinearLayout.LayoutParams(
+                        btnWeight,
+                        btnHeight
+                ));
+
+                // button.setOnClickListener(this);
+                rowLayout.addView(button);
+
+                Log.d("MainActivity", "Button " + row + " " + column + " created with id " + button.getId());
+            }
+
+            boardView.addView(rowLayout);
+        }
     }
 }
